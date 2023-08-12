5e3b7dbbc539531432cbc082c64536d53e7ed84b
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-08-12 17:04:58 +0200

    initializing the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reddit/features/auth/screens/login_screen.dart';
+import 'package:reddit/firebase_options.dart';
 import 'package:reddit/theme/pallete.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
