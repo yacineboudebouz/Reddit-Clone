19f9958ad433fce8582a587913e853b0a139adf6
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-08-21 16:12:04 +0200

    gg

--- lib/core/common/loader.dart
+++ lib/core/common/loader.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Loader extends StatelessWidget {
   const Loader({super.key});
