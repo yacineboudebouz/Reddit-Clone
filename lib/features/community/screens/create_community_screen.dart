79632c18bf2e147300153d529e648461986f02f8
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-08-23 16:40:33 +0200

    create community screen

--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -10,11 +10,52 @@ class CreateCommunityScreen extends ConsumerStatefulWidget {
 }
 
 class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
+  final communityNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    communityNameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Create Community'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const Align(
+                alignment: Alignment.topLeft, child: Text('Community name')),
+            const SizedBox(height: 10.0),
+            const TextField(
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(18),
+                  hintText: 'r/ community name',
+                  filled: true),
+              maxLength: 21,
+            ),
+            const SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              child: const Text(
+                'Create community',
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
